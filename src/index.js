fda8a6fd2f51696eca49c343ca583ed20918b3f3
schrooms <schrooms@gmail.com>
2018-07-10 18:31:10 +0200

    implemented searching

--- src/index.js
+++ src/index.js
@@ -17,8 +17,11 @@ class App extends Component {
       videos: [] ,
       selectedVideo: null
     };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY,term: 'surfboards'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY,term: term}, (videos) => {
       console.log(videos);
       this.setState({
         videos: videos,
@@ -26,10 +29,11 @@ class App extends Component {
       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
